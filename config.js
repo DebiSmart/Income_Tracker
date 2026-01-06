dd19250127b4656baffb2044efce679b9bb7ca2d
DebiSmart <Oren.debi1@gmail.com>
2026-01-06 16:42:57 +0200

    Remove quotes from scriptUrl and sheetUrl

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ window.APP_CONFIG = {
     target: 30000,
 
     // ה-Web App URL שקיבלת מה-Google Apps Script
-    scriptUrl: "https://script.google.com/macros/s/AKfycbyIprpG_h9jXBoRRL6ePgG-4XEUBBYHnl7wPPsb5nM0NrQ8LguhYzjRgOrL5RwoaPH3mA/exec",
+    scriptUrl: https://script.google.com/macros/s/AKfycbyIprpG_h9jXBoRRL6ePgG-4XEUBBYHnl7wPPsb5nM0NrQ8LguhYzjRgOrL5RwoaPH3mA/exec
 
     // הלינק הישיר לגיליון ה-Google Sheets (לצפייה בטבלה)
-    sheetUrl: "https://docs.google.com/spreadsheets/d/18k9uV0DymQ2TZs21MM5NEJGcbTiBprGi3tLAmmQ-ooE/edit?gid=0#gid=0"
+    sheetUrl: https://docs.google.com/spreadsheets/d/18k9uV0DymQ2TZs21MM5NEJGcbTiBprGi3tLAmmQ-ooE/edit?gid=0#gid=0
 };
